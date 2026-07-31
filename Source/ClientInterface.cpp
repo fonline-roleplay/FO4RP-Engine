464d42e5c9bb0504d975e40395b023ce601455f5
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-07-31 21:05:15 +0400

    FO4RP: Don't check `TraceBullet` for attack

--- Source/ClientInterface.cpp
+++ Source/ClientInterface.cpp
@@ -9716,8 +9716,10 @@ void FOClient::CurDraw()
         if( !cr || !Chosen || cr == Chosen )
             break;
 
+        #ifndef FORP_ENGINE
         if( !HexMngr.TraceBullet( Chosen->GetHexX(), Chosen->GetHexY(), cr->GetHexX(), cr->GetHexY(), Chosen->GetAttackDist(), 0.0f, cr, false, NULL, 0, NULL, NULL, NULL, true ) )
             break;
+        #endif
 
         int hit = ScriptGetHitProc( cr, HIT_LOCATION_NONE );
         if( !hit )
