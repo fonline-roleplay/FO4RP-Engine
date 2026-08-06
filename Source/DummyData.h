1402d6997a53a03214fe622b09b3e39662e8e598
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-08-06 16:36:55 +0400

    Client's `uint GetAllMapObjects(ItemCl@[]@ items)`

--- Source/DummyData.h
+++ Source/DummyData.h
@@ -1035,6 +1035,7 @@ struct BindClass
     static void Global_GetMapWidth()            {}
     static void Global_GetMapHeight()           {}
     static void Global_GetMapTime()				{}
+    static void Global_GetAllMapObjects()       {}
     static void Global_GetCurrentCursor()       {}
     static void Global_GetLastCursor()          {}
     static void Global_ChangeCursor()           {}
