d178d1e374fe38420919ecc914dd96d5d1604f58
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-08-18 23:02:38 +0400

    Take global mouse pos, not window-relatively

--- Source/Client.cpp
+++ Source/Client.cpp
@@ -1487,11 +1487,11 @@ void FOClient::ParseMouse()
 {
     // Mouse position
     int mx = 0, my = 0;
-    SDL_GetMouseState( &mx, &my );
+    SDL_GetGlobalMouseState( &mx, &my );
 	int w = 0, h = 0;
 	SDL_GetWindowPosition( MainWindow, &w, &h );
-	GameOpt.MouseX = mx;
-	GameOpt.MouseY = my;
+    GameOpt.MouseX = mx - w;
+    GameOpt.MouseY = my - h;
     GameOpt.MouseX = CLAMP( GameOpt.MouseX, 0, GameOpt.ScreenWidth - 1 );
     GameOpt.MouseY = CLAMP( GameOpt.MouseY, 0, GameOpt.ScreenHeight - 1 );
 
